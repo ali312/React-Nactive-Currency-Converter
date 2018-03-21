b56e1028b50f8216328997551d30fc4b949f70d7
ali312 <ali312@yandex.ru>
2018-03-22 02:09:37 +0600

    Added Last Converted component

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,12 +5,13 @@ import { Container } from "../components/Container";
 import { Logo } from "../components/Logo";
 import { InputWithButton } from "../components/TextInput";
 import { ClearButton } from "../components/Buttons";
+import { LastConverted } from "../components/Text";
 
 const TEMP_BASE_CURRENCY = "USD";
 const TEMP_QUOTE_CURRENCY = "GBP";
 const TEMP_BASE_PRICE = "100";
 const TEMP_QUOTE_PRICE = "79.74";
-const TEMP_CONVERSION_RATE = "0.7974";
+const TEMP_CONVERSION_RATE = 0.7974;
 const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component {
@@ -48,7 +49,16 @@ class Home extends Component {
           onPress={this.handlePressQuoteCurrency}
           value={TEMP_QUOTE_PRICE}
         />
-        <ClearButton text="Reverse Currencies" onPress={this.handleSwapCurrency} />
+        <LastConverted
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          conversionRate={TEMP_CONVERSION_RATE}
+          date={TEMP_CONVERSION_DATE}
+        />
+        <ClearButton
+          text="Reverse Currencies"
+          onPress={this.handleSwapCurrency}
+        />
       </Container>
     );
   }
