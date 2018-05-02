f24bc744cc77236a154521d446072359ae9a80d5
ali312 <ali312@yandex.ru>
2018-05-02 14:13:20 +0600

    Added currency list actions

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import { FlatList, StatusBar, View } from 'react-native';
+import React, { Component } from "react";
+import { FlatList, StatusBar, View } from "react-native";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-import { ListItem, Separator } from '../components/List';
-import currencies from '../data/currencies';
+import { ListItem, Separator } from "../components/List";
+import currencies from "../data/currencies";
 
-const TEMP_CURRENT_CURRENCY = 'CAD';
+import { changeBaseCurrency, changeQuoteCurrency } from "../actions/currencies";
+
+const TEMP_CURRENT_CURRENCY = "CAD";
 
 class CurrencyList extends Component {
   static propTypes = {
-    navigation: PropTypes.object
+    navigation: PropTypes.object,
+    dispatch: PropTypes.func,
   };
 
-  handlePress = () => {
+  handlePress = (currency) => {
+    const { type } = this.props.navigation.state.params;
+    if (type === 'base') {
+      this.props.dispatch(changeBaseCurrency(currency))
+    } else {
+      this.props.dispatch(changeQuoteCurrency(currency))
+    }
+
     this.props.navigation.goBack(null);
   };
 
@@ -26,7 +37,7 @@ class CurrencyList extends Component {
             <ListItem
               text={item}
               selected={item === TEMP_CURRENT_CURRENCY}
-              onPress={this.handlePress}
+              onPress={() => this.handlePress(item)}
             />
           )}
           keyExtractor={item => item}
@@ -37,4 +48,4 @@ class CurrencyList extends Component {
   }
 }
 
-export default CurrencyList;
+export default connect()(CurrencyList);
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -23,7 +23,7 @@ class Home extends Component {
     amount: PropTypes.number,
     conversionRate: PropTypes.number,
     isFetching: PropTypes.bool,
-    lastConvertedDate: PropTypes.object,
+    lastConvertedDate: PropTypes.object
   };
 
   handleChangeText = amount => {
@@ -43,11 +43,17 @@ class Home extends Component {
   };
 
   handlePressBaseCurrency = () => {
-    this.props.navigation.navigate("CurrencyList", { title: "Base Currency" });
+    this.props.navigation.navigate("CurrencyList", {
+      title: "Base Currency",
+      type: "base"
+    });
   };
 
   handlePressQuoteCurrency = () => {
-    this.props.navigation.navigate("CurrencyList", { title: "Quote Currency" });
+    this.props.navigation.navigate("CurrencyList", {
+      title: "Quote Currency",
+      type: "quote"
+    });
   };
 
   render() {
@@ -103,7 +109,9 @@ const mapStateToProps = state => {
     amount: state.currencies.amount,
     conversionRate: rates[quoteCurrency] || 0,
     isFetching: conversionSelector.isFetching,
-    lastConvertedDate: conversionSelector.date ? new Date(conversionSelector.date) : new Date()
+    lastConvertedDate: conversionSelector.date
+      ? new Date(conversionSelector.date)
+      : new Date()
   };
 };
 
